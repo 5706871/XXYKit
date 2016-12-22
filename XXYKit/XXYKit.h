e49f393c36959f227c4376dfdaa3738cfc0aef43
XY <5706871@qq.com>
2016-12-22 14:58:53 +0800

    初始化项目

---
+++ XXYKit/XXYKit.h
@@ -0,0 +1,19 @@
+//
+//  XXYKit.h
+//  XXYKit
+//
+//  Created by fatty on 2016/12/22.
+//  Copyright © 2016年 xxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XXYKit.
+FOUNDATION_EXPORT double XXYKitVersionNumber;
+
+//! Project version string for XXYKit.
+FOUNDATION_EXPORT const unsigned char XXYKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XXYKit/PublicHeader.h>
+
+
